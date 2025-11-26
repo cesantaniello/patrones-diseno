f49a85c5fb3db8620f557920e198963520c5b5bb
Carlos Eduardo Santaniello Lozano <carlos-eduardo.santaniello-lozano@capgemini.com>
2025-11-26 14:01:50 +0100

    Factory method example

--- 01-creacionales/02-factory-method.ts
+++ 01-creacionales/02-factory-method.ts
@@ -29,6 +29,12 @@ class ChickenBurger implements Hamburger {
     }
 }
 
+class BeanBurger implements Hamburger {
+    prepare(): void {
+        console.log(`Preparando una hamburguesa de frijoles...`);
+    }
+}
+
 abstract class BurgerFactory {
     abstract createBurger(): Hamburger;
 
@@ -49,3 +55,34 @@ class ChickenBurgerFactory extends BurgerFactory {
         return new ChickenBurger();
     }
 }
+
+class BeanBurgerFactory extends BurgerFactory {
+    createBurger(): Hamburger {
+        return new BeanBurger();
+    }
+}
+
+function main() {
+
+    let factory: BurgerFactory;
+    const burgerType = prompt('¿Qué tipo de hamburguesa deseas? (veggie/chicken/bean): ')
+    
+    switch (burgerType) {
+        case 'veggie':
+            factory = new VeggieBurgerFactory();
+            break;
+        case 'chicken':
+            factory = new ChickenBurgerFactory();
+            break;
+        case 'bean':
+            factory = new BeanBurgerFactory();
+            break;
+        default:
+            console.log('Tipo de hamburguesa no válido.');
+            return;
+    }
+    factory.orderBurger();
+
+}
+
+main();
