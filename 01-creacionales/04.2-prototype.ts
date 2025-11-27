c56d70f4a4c6b7f331743ff42e9b1f17d54e234e
Carlos Eduardo Santaniello Lozano <carlos-eduardo.santaniello-lozano@capgemini.com>
2025-11-27 14:51:54 +0100

    Prototype pattern homework

--- 01-creacionales/04.2-prototype.ts
+++ 01-creacionales/04.2-prototype.ts
@@ -17,13 +17,22 @@ class Pokemon {
   attacks: string[];
 
   constructor(name: string, type: string, level: number, attacks: string[]) {
-    throw new Error('Method not implemented.');
+    this.name = name;
+    this.type = type;
+    this.level = level;
+    this.attacks = attacks;
   }
 
   // Método para clonar el Pokémon
   clone(): Pokemon {
     // Los ataques deben de evitar pasarse por referencia, es decir, no deben de ser el mismo arreglo.
     // Completar: Debe devolver un nuevo Pokémon con los mismos atributos
+    return new Pokemon(
+      this.name,
+      this.type,
+      this.level,
+      [...this.attacks] // Crear una copia del arreglo de ataques
+    );
   }
 
   displayInfo(): void {
@@ -49,3 +58,16 @@ class Pokemon {
 
 // basePokemon.displayInfo(); // Aquí no debe de aparecer "Lanzallamas"
 // clone1.displayInfo();
+
+function main() {
+  const basePokemon = new Pokemon("Charmander", "Fuego", 1, ["Llamarada", "Arañazo"]);
+  const clone1 = basePokemon.clone();
+  clone1.name = "Charmeleon";
+  clone1.level = 16;
+  clone1.attacks.push("Lanzallamas");
+
+  basePokemon.displayInfo(); // Aquí no debe de aparecer "Lanzallamas"
+  clone1.displayInfo();
+}
+
+main();
