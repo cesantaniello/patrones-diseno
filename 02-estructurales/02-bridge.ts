1212b3fbfa5c33d428fb74329b04c1c45d717f72
Carlos Eduardo Santaniello Lozano <carlos-eduardo.santaniello-lozano@capgemini.com>
2025-12-04 16:43:51 +0100

    Bridge pattern

--- 02-estructurales/02-bridge.ts
+++ 02-estructurales/02-bridge.ts
@@ -9,3 +9,46 @@
  *
  * https://refactoring.guru/es/design-patterns/bridge
  */
+
+interface Ability {
+    useAbility(): void;
+}
+
+class FireAbility implements Ability {
+    useAbility(): void {
+        console.log('Using fire ability!');
+    }
+}
+class IceAbility implements Ability {
+    useAbility(): void {
+        console.log('Using ice ability!');
+    }
+}
+
+abstract class Character {
+    protected ability: Ability;
+
+    constructor(ability: Ability) {
+        this.ability = ability;
+    }
+
+    setAbility(ability: Ability) {
+        this.ability = ability;
+    }
+
+    abstract performAbility(): void;
+}
+
+class Warrior extends Character {
+    performAbility(): void {
+        console.log('Warrior:');
+        this.ability.useAbility();
+    }
+}
+
+class Mage extends Character {
+    performAbility(): void {
+        console.log('Mage:');
+        this.ability.useAbility();
+    }
+}
