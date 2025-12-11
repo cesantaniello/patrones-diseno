1329ccf3bb68b7442e776e52de73b454a38feb59
Carlos Eduardo Santaniello Lozano <carlos-eduardo.santaniello-lozano@capgemini.com>
2025-12-11 16:24:56 +0100

    Flyweight pattern

--- 02-estructurales/06-flyweight.ts
+++ 02-estructurales/06-flyweight.ts
@@ -8,3 +8,34 @@
  *
  * https://refactoring.guru/es/design-patterns/flyweight
  */
+
+interface Location {
+    display(coordinates: {x: number, y: number}): void;
+}
+
+class LocationIcon implements Location {
+
+    private type: string;
+    private iconImage: string;
+
+    constructor(type: string, iconImage: string) {
+        this.type = type;
+        this.iconImage = iconImage;
+    }
+
+    display(coordinates: {x: number, y: number}): void {
+        console.log(`Mostrando icono de tipo ${this.type} en (${coordinates.x}, ${coordinates.y}) con imagen ${this.iconImage}`);
+    }
+}
+
+class LocationFactory {
+    private icons: Record<string, LocationIcon> = {};
+
+    getLocationIcon(type: string, iconImage: string): LocationIcon {
+        const key = `${type}-${iconImage}`;
+        if (!this.icons[key]) {
+            this.icons[key] = new LocationIcon(type, iconImage);
+        }
+        return this.icons[key];
+    }
+}
