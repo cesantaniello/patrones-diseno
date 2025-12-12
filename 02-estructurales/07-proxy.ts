afb9c0b1c0d180bfadb9a3e87c56988a9a905c9f
Carlos Eduardo Santaniello Lozano <carlos-eduardo.santaniello-lozano@capgemini.com>
2025-12-12 13:43:22 +0100

    Proxy pattern

--- 02-estructurales/07-proxy.ts
+++ 02-estructurales/07-proxy.ts
@@ -10,3 +10,47 @@
  * https://refactoring.guru/es/design-patterns/proxy
  *
  */
+
+class Player {
+
+    name: string;
+    level: number;
+
+    constructor(name: string, level: number) {
+        this.name = name;
+        this.level = level;
+    }   
+}
+
+interface Room {
+    enter(player: Player): void;
+}
+
+class BasicRoom implements Room {
+    enter(player: Player): void {
+        console.log(`${player.name} has entered the basic room.`);
+    }
+}
+
+class SecretRoom implements Room {
+    enter(player: Player): void {
+        console.log(`${player.name} has entered the secret room.`);
+    }
+}
+
+class MagicPortal implements Room {
+
+    private secretRoom: SecretRoom;
+
+    constructor(room: SecretRoom) {
+        this.secretRoom = room;
+    }
+
+    enter(player: Player): void {
+        if (player.level >= 10) {
+            this.secretRoom.enter(player);
+        }
+        console.log(`${player.name} is not allowed to enter the secret room.`);
+    }
+
+}
