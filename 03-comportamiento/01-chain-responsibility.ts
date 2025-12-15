629018779b87e67ef1ee8408e295df4886879e00
Carlos Eduardo Santaniello Lozano <carlos-eduardo.santaniello-lozano@capgemini.com>
2025-12-15 13:45:28 +0100

    Change of responsibility pattern

--- 03-comportamiento/01-chain-responsibility.ts
+++ 03-comportamiento/01-chain-responsibility.ts
@@ -9,3 +9,58 @@
  *
  * https://refactoring.guru/es/design-patterns/chain-of-responsibility
  */
+
+interface Handler {
+  setNext(handler: Handler): Handler;
+  handle(request: string): void;
+}
+
+abstract class BaseHandler implements Handler {
+    private nextHandler?: Handler;
+
+    public setNext(handler: Handler): Handler {
+        this.nextHandler = handler;
+        return handler;
+    }
+
+    public handle(request: string): void {
+        if (this.nextHandler) {
+            this.nextHandler.handle(request);
+        }
+    }
+}
+
+class BasicSupport extends BaseHandler {
+
+    override handle(request: string): void {
+        if (request === 'basic') {
+            console.log('BasicSupport: Manejo la solicitud de soporte básico.');
+            return;
+        }
+        console.log('BasicSupport: No puedo manejar la solicitud, paso al siguiente.');
+        super.handle(request);
+    }
+}
+
+class AdvancedSupport extends BaseHandler {
+    override handle(request: string): void {
+        if (request === 'advanced') {
+            console.log('AdvancedSupport: Manejo la solicitud de soporte avanzado.');
+            return;
+        }
+        console.log('AdvancedSupport: No puedo manejar la solicitud, paso al siguiente.');
+        super.handle(request);
+    }
+}
+
+class ExpertSupport extends BaseHandler {
+    override handle(request: string): void {
+        if (request === 'expert') {
+            console.log('ExpertSupport: Manejo la solicitud de soporte experto.');
+            return;
+        }
+
+        console.log('ExpertSupport: No puedo manejar la solicitud, paso al siguiente.');
+        super.handle(request);
+    }
+}
