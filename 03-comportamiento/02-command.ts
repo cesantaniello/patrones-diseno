bd93fbd50ec9f8c520eb5400a640e0f6da248dc2
Carlos Eduardo Santaniello Lozano <carlos-eduardo.santaniello-lozano@capgemini.com>
2025-12-16 14:36:30 +0100

    Command pattern

--- 03-comportamiento/02-command.ts
+++ 03-comportamiento/02-command.ts
@@ -12,3 +12,79 @@
  *
  *
  */
+
+interface Command {
+    execute(): void;
+}
+
+class Light {
+    turnOn() {
+        console.log("La luz está encendida");
+    }
+
+    turnOff() {
+        console.log("La luz está apagada");
+    }
+}
+
+class Fan {
+    turnOn() {
+        console.log("El ventilador está encendido");
+    }
+
+    turnOff() {
+        console.log("El ventilador está apagado");
+    }
+}
+
+class LightOnCommand implements Command {
+
+    constructor(private light: Light) {}
+
+    execute(): void {
+        this.light.turnOn();
+    }
+}
+
+class LightOffCommand implements Command {
+
+    constructor(private light: Light) {}
+
+    execute(): void {
+        this.light.turnOff();
+    }
+}
+
+class FanOnCommand implements Command {
+
+    constructor(private fan: Fan) {}
+
+    execute(): void {
+        this.fan.turnOn();
+    }
+}
+
+
+class FanOffCommand implements Command {
+
+    constructor(private fan: Fan) {}
+
+    execute(): void {
+        this.fan.turnOff();
+    }
+}
+
+class RemoteControl {
+    private commands: Record<string, Command> = {};
+
+    setCommand(button: string, command: Command) {
+        this.commands[button] = command;
+    }
+
+    pressButton(button: string) {
+        if (this.commands[button]) {
+            this.commands[button].execute();
+        }
+    }
+}
+
