23a5f42babe27aa91353d7aa022e9b6ff3a9d88a
Carlos Eduardo Santaniello Lozano <carlos-eduardo.santaniello-lozano@capgemini.com>
2026-01-29 17:01:18 +0100

    State pattern

--- 03-comportamiento/07-state.ts
+++ 03-comportamiento/07-state.ts
@@ -18,3 +18,63 @@
  *  * Entregando Producto,
  * * y su comportamiento varía dependiendo del estado actual.
  */
+
+interface State {
+
+    name: string;
+
+    insertMoney(amount: number): void;
+    selectProduct(productCode: string): void;
+    dispenseProduct(): void;
+}
+
+class VendingMachine {
+    private state: State;
+
+    constructor() {
+        
+    }
+
+    insertMoney(amount: number): void {
+        this.state.insertMoney(amount);
+    }
+
+    selectProduct(productCode: string): void {
+        this.state.selectProduct(productCode);
+    }
+
+    dispenseProduct(): void {
+        this.state.dispenseProduct();
+    }
+
+    getStateName(): string {
+        return this.state.name;
+    }
+}
+
+class WaitingForMoneyState implements State {
+    
+    private name: string = "WaitingForMoney";
+    private vendingMachine: VendingMachine;
+
+    constructor(vendingMachine: VendingMachine) {
+        this.vendingMachine = vendingMachine;
+    }
+
+    insertMoney(): void {
+        console.log('Now you can choose a product')
+        throw new Error("Method not implemented.");
+
+        //this.vendingMachine.setState();
+    }
+
+
+    selectProduct(): void {
+        console.log('You must introduce money');
+    }
+
+    dispenseProduct(): void {
+        console.log('You must introduce money');
+    }
+
+}
